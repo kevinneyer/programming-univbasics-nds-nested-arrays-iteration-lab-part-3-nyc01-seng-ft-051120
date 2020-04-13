76c29f1d0cb0ae260232b8d229a9a89d4af84da1
Kevin Neyer <kevinneyer@gmail.com>
2020-04-13 16:51:54 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -10,7 +10,7 @@ def join_nested_strings(src)
   while count < src.length do
     i = 0 
     while i < src[count].length do
-      if src[count][i] 0 
+      if src[count][i] 
         message += src[count][i].to_s + " "
       end 
       i += 1 
